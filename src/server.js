a9feaf4e2350923100597e362918a9d237486a70
Anisa Hikmatuni <anisa.hikmatuni@gmail.com>
2023-03-26 22:19:16 +0200

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -15,9 +15,4 @@ const init = async () => {
   console.log(`Server running on ${server.info.uri}`);
 };
 
-process.on('unhandledRejection', (err) => {
-  console.log(err);
-  process.exit(1);
-});
-
 init();
