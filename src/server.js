f08c14fa7f963f226dab58de094f1e4ae92ba2c5
Anisa Hikmatuni <anisa.hikmatuni@gmail.com>
2023-03-26 20:40:55 +0200

    initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
